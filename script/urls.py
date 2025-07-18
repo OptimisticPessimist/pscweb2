d382103f32732a8938fb453c27dacd056da1aa5c
OptimisticPessimist <>
2025-07-18 22:32:25 +0900

    feat: azure対応

--- script/urls.py
+++ script/urls.py
@@ -3,23 +3,27 @@ from . import views
 
 app_name = 'script'
 urlpatterns = [
-    
+
     # /scrpt/ -> Script List
     path('', views.ScriptList.as_view(), name='scrpt_list'),
-    
+
     # /scrpt/scrpt_create/ -> Script Create
     path('scrpt_create/', views.ScriptCreate.as_view(), name='scrpt_create'),
     # /scrpt/scrpt_update/1/ -> Script #1 Update
     path('scrpt_update/<int:pk>/', views.ScriptUpdate.as_view(),
-        name='scrpt_update'),
+         name='scrpt_update'),
     # /scrpt/scrpt_detail/1/ -> Script #1 Detail
     path('scrpt_detail/<int:pk>/', views.ScriptDetail.as_view(),
-        name='scrpt_detail'),
+         name='scrpt_detail'),
     # /scrpt/scrpt_viewer/1/ -> Script #1 Viewer
     path('scrpt_viewer/<int:pk>/', views.ScriptViewer.as_view(),
-        name='scrpt_viewer'),
-    
+         name='scrpt_viewer'),
+
+    # --- この行を追加 ---
+    # /scrpt/scrpt_pdf/1/ -> Download Script #1 as PDF
+    path('scrpt_pdf/<int:pk>/', views.ScriptPDFDownloadView.as_view(), name='scrpt_pdf'),
+
     # /scrpt/prod_from_scrpt/1/ -> Create Production from Script #1
     path('prod_from_scrpt/<int:scrpt_id>/', views.ProdFromScript.as_view(),
-        name='prod_from_scrpt'),
+         name='prod_from_scrpt'),
 ]
